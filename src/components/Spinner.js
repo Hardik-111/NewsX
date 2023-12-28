5cfd80f08a00e9f6bd73135c38314226dc9f9c05
Hardik_112 <89783619+Hardik-111@users.noreply.github.com>
2023-12-28 09:10:53 +0530

    added dark and light mode on toggling

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,13 +1,14 @@
 import React  from 'react'
 import loading from './loading.gif'
 
-const Spinner=()=> {
-   
+const Spinner=(props)=> {
         return (
+            <div className={`text-center ${props.darkMode ? "text-light" : ""}`}>
             <div className="text-center">
                 <img className="my-3" src={loading} alt="loading" />
+            </div>
             </div>
         )
     
 }
-export default Spinner
+export default Spinner;
